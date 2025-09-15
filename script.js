469029a1324a019cfab999a411b94b0bc81aa106
PyotrWrangel <maerlyn@hotmail.it>
2025-09-15 18:02:28 +0200

    1) Aggiunto controllo utente loggato, 2) aggiunto fetch per logout, 3)aggiunte condizioni di verifica ai query selector per prevenire errori null

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+console.log('script caricato');
+
 const register = document.querySelector("#registerButton");
+if (register) {
 register.addEventListener("click", generateFormRegister);
+}
+
+const Logout = document.querySelector("#logoutButton");
+if(Logout) {
+Logout.addEventListener("click", logout);
+}
 
 //REGISTRAZIONE
 
 function generateFormRegister() {
   const html = document.querySelector("#registerForm").innerHTML = `
-                <form id="register" action="register.php" method="post">
+                <form id="register" method="post">
                 <label for="userName"> Username: </label>
                 <input type="text" name="userName" id="username">
                 <label for="email"> Email: </label>
@@ -17,7 +26,9 @@ function generateFormRegister() {
                 `;
   // document.insertAdjacentHTML("afterend", html)
   const formRegister = document.querySelector("#register");
+  if (formRegister) {
   formRegister.addEventListener("submit", addUtente);
+  }
 }
 
 function addUtente(e) {
@@ -26,7 +37,7 @@ function addUtente(e) {
   console.log(form);
   const fd = new FormData(form);
 
-  fetch("./querys/register.php", {
+  fetch("/corsoPhp/phpmysql/movieManagement/querys/register.php", {
     method: "POST",
     header: {
       "Content-type": "application/json",
@@ -42,20 +53,24 @@ function addUtente(e) {
 //LOGIN
 
 const login = document.querySelector("#loginButton");
+if(login) {
 login.addEventListener("click", generateFormLogin);
+}
 
 function generateFormLogin() {
    const html = document.querySelector("#loginForm").innerHTML = `
-     <form id="loginUtente" action="login.php" method="POST">
+     <form id="loginUtente" method="POST">
                 <label for="email"> Email: </label>
-                <input type="email" name="email" id="email">
+                <input type="email" name="email" id="email" required>
                 <label for="password">Password: </label>
-                <input type="password" name="password" id="password">
+                <input type="password" name="password" id="password" required>
                 <input type="submit" name="login" value="Login">
                 </form>
     `;
     const form = document.querySelector("#loginUtente");
+    if(form) {
     form.addEventListener("submit", loginUtente);
+    }
 }
 
 function loginUtente(e) {
@@ -65,7 +80,7 @@ function loginUtente(e) {
   const fd = new FormData(formLogin);
   console.log(fd);
 
-  fetch ("./querys/login.php", {
+  fetch ("/corsoPhp/phpmysql/movieManagement/querys/login.php", {
     method: 'POST',
     // headers: {
     //   "Content-type": "application/json",
@@ -76,12 +91,15 @@ function loginUtente(e) {
 
       .then((response) => response.json())
       .then(data => {
-        console.log("Risposta JSON: ", data);
+        console.log("Risposta JSON: ", data.response);
 
-        if(data.response === 1) {
-          document.location.href = './pages/dashboard.php';
-        } else {
-          alert("non reindirizzato");
+       if(data.response == 1) {
+        console.log('entro nella prima condizione');
+         document.location.href = './pages/dashboard.php';
+       } else {
+         console.log('entro nella seconda condizione');
+                  document.querySelector("#responseLogin").innerHTML = `
+          <p style= " border: solid; border-color: red; background-color: rgba(255, 0, 0, 0.5); padding: 4px; border-radius: 6px;">Email o Password errate</p>`;
         }
       })
   .catch(err => console.error("errore fetch:", err));
@@ -89,13 +107,31 @@ function loginUtente(e) {
 
 //fetch controllo utente loggato
 
-fetch("./pages/dashboard.php")
+fetch("/corsoPhp/phpmysql/movieManagement/pages/dashboard.php")
 .then((response) => response.json())
 .then(data => {
-  if(data.response === 0) {
-    document.location.href = './index.php';
+  if(data.response === 0 && !window.location.pathname.endsWith("index.php")) {
+    window.location.href = '/corsoPhp/phpmysql/movieManagement/index.php';
   }
 })
 
+//fetch utente sloggato
+function logout(e) {
+e.preventDefault();
+fetch("/corsoPhp/phpmysql/movieManagement/querys/logout.php")
+.then((response) => response.json())
+.then(data => {
+  if(data.response === 1) {
+  document.location.href = "/corsoPhp/phpmysql/movieManagement/index.php"
+  }
+})
+.catch (err => console.error("errore fetch: ", err));
+}
 
+    //debug risposta grezza
+      // .then(res => {
+  //   console.log("status", res.status);
+  //   return res.text();
+  // })
+  // .then(txt => console.log("risposta grezza:", txt))
 
