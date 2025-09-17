e6e216f6e380b4af5ae670215bcf3265c427f027
PyotrWrangel <maerlyn@hotmail.it>
2025-09-17 12:56:16 +0200

    risolto problema fetch registrazione

--- script.js
+++ script.js
@@ -38,16 +38,20 @@ function addUtente(e) {
     body: fd,
     
   })
-.then(res => res.text())
-.then(txt => {
-  console.log("📜 Risposta grezza:", txt);
-  try {
-    const data = JSON.parse(txt);   // prova a parsare
-    console.log("✅ JSON valido:", data);
-  } catch (err) {
-    console.error("❌ JSON non valido:", err.message);
+.then(res => res.json())
+.then(data => {
+  if (data.response === 0) {
+    document.querySelector("#responseRegister").innerHTML = `
+       <p style= " border: solid; border-color: red; background-color: rgba(255, 0, 0, 0.4); padding: 4px; border-radius: 6px;">Email già esistente</p>`;
+  } else if (data.response === 2) {
+    document.querySelector("#responseRegister").innerHTML = `
+           <p style= " border: solid; border-color: blue; background-color: rgba(79, 161, 255, 0.5); padding: 4px; border-radius: 6px;">Registrazione avvenuta con successo</p>`;
+  } else {
+        document.querySelector("#responseRegister").innerHTML = `
+       <p style= " border: solid; border-color: green; background-color: rgba(82, 255, 38, 0.4); padding: 4px; border-radius: 6px;">Errore nel server, riprova più tardi</p>`;
   }
-});
+})
+ .catch(err => console.error("errore fetch registrazione:", err));
 }
 
 //LOGIN
