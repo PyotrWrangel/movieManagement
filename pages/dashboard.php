14a7e8aa0776b60ba12077dbce87650fd9669d3d
PyotrWrangel <maerlyn@hotmail.it>
2025-09-17 12:05:26 +0200

    aggiunto messaggio utente registrato

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -6,16 +6,28 @@ error_log("SESSION in dashboard" . print_r($_SESSION, true));
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style.css">
     <title>Document</title>
 </head>
-<body>
-    <p>Benvenuto</p>
-    <h1><?php echo $_SESSION['userName'] ?></h1>
-    <button id="logoutButton">Logout</button>
 
-<script src="checklogged.js"></script>
+<body>
+    <header class="first-header">
+        <div class="header-button">
+            <button class="logoutButton" id="logoutButton">Logout</button>
+        </div>
+        <div class="username">
+            <p class="header-p">Benvenuto
+            <h1 class="header-title"><?php echo $_SESSION['userName'] ?></h1>
+            </p>
+        </div>
+    </header>
+
+
+    <script src="checklogged.js"></script>
 </body>
+
 </html>
